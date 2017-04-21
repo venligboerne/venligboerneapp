9caf5f4316b4c8fcadf768bbaf8ff47e878bb0be
Mitchell <mitchell4408@hotmail.com>
2017-04-21 16:34:55 -0700

    test

--- index.ios.js
+++ index.ios.js
@@ -17,7 +17,7 @@ export default class venligboerneapp extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native test!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
